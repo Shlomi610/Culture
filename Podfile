8b8e8aaa23471b8dc97cc1e9cbec159fa71977e1
Shlomi Sharon <sharon.shlomi@gmail.com>
2020-11-07 19:21:44 +0200

    pods

---
+++ Podfile
@@ -0,0 +1,5 @@
+
+target 'Culture' do
+  use_frameworks!
+  pod 'Alamofire', '~> 5.2'
+end
---
+++ Pods/Target Support Files/Pods-Culture/Pods-Culture-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Culture : NSObject
+@end
+@implementation PodsDummy_Pods_Culture
+@end
